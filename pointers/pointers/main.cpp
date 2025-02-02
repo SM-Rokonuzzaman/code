f0874a66b61a9346d14d71bb11cf8c585f1bea4a
Rokon <rokon.acps@gmail.com>
2025-02-02 16:11:21 +0600

    pointer code

--- pointers/pointers/main.cpp
+++ pointers/pointers/main.cpp
@@ -1,18 +1,35 @@
+/*
+a pointer is a variable that stores the memory address of another variable.
+instead of holding the direct value, it holds the address where the value is stored in the memory.
+
+there are 2 important operators
+    -Dereferencing operator (*):used to declare pointer variable and access
+        the value stored in the address.
+    -Address operator (&*:used to returns the address of a variable or to
+        access the address of a variable to a pointer.
+
+
+*/
+
 #include <iostream>
 
-int *ptr;
 
-int value;
+
+int m = 100;    // taking an integer variable
+
+int *ptr = &m;  // pointer variable that stores the address of m variable.
 
 using namespace std;
 
 int main()
 {
-    value = 10;
-    ptr = &value;
 
-    cout << "Value: " << value << endl;
-    cout << "Value using pointer: " << *ptr << endl;
+    cout << "Value of m variable (m): " << m << endl;
+    cout << "Value of m variable using (*ptr) : " << *ptr << endl;
+    cout << "Address of m variable (&m)" << &m << endl;
+    cout << "Address of m variable using (ptr)" << ptr << endl;
+
+
 
     cout << "Hello world!" << endl;
     return 0;
