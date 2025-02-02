548a0844c3242755059756ae5dfed8bd29dc801c
Rokon <rokon.acps@gmail.com>
2025-02-02 15:59:26 +0600

    pointer code added

--- pointers/pointers/main.cpp
+++ pointers/pointers/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
+int *ptr;
+
+int value;
+
 using namespace std;
 
 int main()
 {
+    value = 10;
+    ptr = &value;
+
+    cout << "Value: " << value << endl;
+    cout << "Value using pointer: " << *ptr << endl;
+
     cout << "Hello world!" << endl;
     return 0;
 }
