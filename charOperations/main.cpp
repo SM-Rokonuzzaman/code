8510a67a6ffb6a5781df739f28c01fcf256d7c60
Rokon <rokon.acps@gmail.com>
2025-02-05 16:56:49 +0600

    commit

--- charOperations/main.cpp
+++ charOperations/main.cpp
@@ -5,14 +5,20 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
+
 
 using namespace std;
 
 int main()
 {
-    char str1[20] = "abcd", str2[20] = "ABCD", str3[20]="xyz";
-    strcpy(str3 + 2, strcat(str1 + 2, str2 + 1));
-    printf("%s.\n",str3);
+    struct Test1
+{
+    int b;
+    double c;
+    long d;
+}data, *p;
 
+cout << sizeof(data) << endl;;
     return 0;
 }
