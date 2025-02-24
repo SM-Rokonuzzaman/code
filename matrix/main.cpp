be6c17f53531ad650489b3b675d5f8f7eaefcd9f
Rokon <rokon.acps@gmail.com>
2025-02-24 12:17:29 +0600

    hhhh

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include <fstream>
 
 using namespace std;
@@ -23,6 +22,7 @@ void write_file(string res)
 int matA[rowA][colA]=   {{1,2},
                         {4,5},
                         {7,8}};
+int (*mataptr)[2] = matA;
 
 int matB[rowB][colB]=   {{1,2,3},
                         {4,5,6},
@@ -31,6 +31,16 @@ int matB[rowB][colB]=   {{1,2,3},
 int res[3][3];
 
 //int (*matAptr)[3]=matA;
+
+struct employee
+{
+    int age;
+    const char * name;
+    int salay;
+    int *matrix[2];
+
+
+};
 int main()
 {
 
@@ -58,33 +68,17 @@ int main()
             cout << res[i][j]<<" ";
         }cout << endl;
     }
-=======
-
-using namespace std;
-
-int matxA[3][3] = {{3, 12, 4},{4, 6, 8},{1, 0, 2}};
-int matxB[3][3] = {{7, 3, 8},{11, 9, 5},{6, 8, 4}};
-int (*ptrA)[3]= matxA;
-int (*ptrB)[3]= matxB;
 
 
-void print_matx(int *point )
-{
 
-    for(int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << *point << ",";
-        }
-        cout << endl;
-    }
-}
+     struct employee e1 = {25, "Rokonuzzaman",20};
+     struct employee e2 = {20, "Sowrov Das",25};
 
 
-int main()
-{
-    print_matx(*ptrA);
->>>>>>> 3a73c755728fe9bdc1dcfacada69e40cfb3940f8
+    cout << e2.age << endl;
+    cout << e1.matrix << endl;
+
+
+
     return 0;
 }
