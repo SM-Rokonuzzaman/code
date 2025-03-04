66aec549110e5bdfb814012d01199f4788f25898
S M Rokonuzzaman <rokon.acps@gmail.com>
2025-03-04 23:08:58 +0600

    kk

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
 void printQueue(queue<int>queue)
@@ -15,16 +16,26 @@ int main()
 {
     //push(), pop(),size(), front(),back()
     queue<int>myQueue;
+    queue<uint8_t>myQ;
 
     myQueue.push(1);
     myQueue.push(2);
     myQueue.push(3);
     myQueue.push(4);
 
+    myQ.push(10);
+    myQ.push(20);
+    myQ.push(30);
+
     cout << "The size of the Queue is: " << myQueue.size() << endl;
     cout << "The first element is: " << myQueue.front() << endl;
     cout << "The last element is: " << myQueue.back() << endl;
 
     printQueue(myQueue);
+    while(!myQ.empty())
+    {
+        printf("Element : %d\n",myQ.front());
+        myQ.pop();
+    }
     return 0;
 }
